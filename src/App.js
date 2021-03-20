61842007989cf7071025a8d1d6cc63aa36a43bf1
musaabdullah <musaabdalwahed@gmail.com>
2021-03-20 15:34:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import  * as AI from "react-icons/ai";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+             <div className="form">
+               <div className="form__logo">
+                 FEAHTED
+               </div>
+               <form>
+                 <input type="text" placeholder="Full Name"/>
+                 <input type="password" placeholder="Password" />
+                 <input type="password" placeholder="Confirm Password"/>
+                 <input type="email" placeholder="Email" />
+                 <button onClick={(e) => {e.preventDefault()}} className="form__btn">Sign Up</button>
+               </form>
+             <div className="form__sign">Have an account?<a href="#">Sign In</a></div>
+             <div className="form__social">
+               <div className="social__title">
+                 or you can sign in with
+               </div>
+               <div className="social__items">
+                 <a href="#"><AI.AiFillGithub/></a>
+                 <a href="#"><AI.AiFillGoogleCircle/></a>
+                 <a href="#"><AI.AiFillFacebook/></a>
+               </div>
+             </div>
+             </div>
     </div>
   );
 }
